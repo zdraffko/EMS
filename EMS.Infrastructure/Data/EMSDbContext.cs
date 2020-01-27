86ff7925f0c99fa66022d075cc3fa5106d7c56a1
zdraffko <zdravkomb1@gmail.com>
2020-01-27 17:53:19 +0200

    Added EF Core and registered a DbContext

---
+++ EMS.Infrastructure/Data/EMSDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Infrastructure.Data
+{
+    public class EMSDbContext : DbContext
+    {
+        public EMSDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
--- EMS.Web/Startup.cs
+++ EMS.Web/Startup.cs
@@ -1,10 +1,11 @@
+using EMS.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace EMS.Web
 {
@@ -19,7 +20,13 @@ namespace EMS.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews(options => options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+            services.AddControllersWithViews(options =>
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute())
+                );
+
+            services.AddDbContextPool<EMSDbContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DevConnection"))
+                );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
