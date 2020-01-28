6b1a99ab4ccc3636a835665643923a523d9946f9
zdraffko <zdravkomb1@gmail.com>
2020-01-28 13:26:17 +0200

    Created repository contracts

---
+++ EMS.Core/Interfaces/IManagerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using EMS.Core.Entities.ManagerAggregate;
+
+namespace EMS.Core.Interfaces
+{
+    public interface IManagerRepository : IRepository<Manager>
+    {
+        Task HireEmployeeAsync(Guid managerId, string firstName, string lastName, int age, string email, string department, decimal salary);
+
+        Task FireEmployeeAsync(Guid managerId, Guid employeeId);
+
+        Task PromoteEmployeeAsync(Guid managerId, Guid employeeId, decimal promotionAmount);
+
+        Task DemoteEmployeeAsync(Guid managerId, Guid employeeId, decimal demotionAmount);
+    }
+}
---
+++ EMS.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EMS.Core.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : IAggregateRoot
+    {
+        Task AddAsync(TEntity entity);
+
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
